a57998f9aecf4d9067274327964eb2d03256e285
jordi <jordicalatayudcastro@gmail.com>
2021-04-10 10:38:34 +0200

    optimització del codi

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,42 +9,45 @@ var wheelForm = document.querySelector('#wheelForm');
 var plate = document.querySelector("#plate");
 var brand = document.querySelector("#brand");
 var color = document.querySelector("#color");
-//Inputs wheel Form
-var marcaRueda1 = document.querySelector("#marcaRueda1");
-var marcaRueda2 = document.querySelector("#marcaRueda2");
-var marcaRueda3 = document.querySelector("#marcaRueda3");
-var marcaRueda4 = document.querySelector("#marcaRueda4");
-var diametro1 = document.querySelector("#diametro1");
-var diametro2 = document.querySelector("#diametro2");
-var diametro3 = document.querySelector("#diametro3");
-var diametro4 = document.querySelector("#diametro4");
 // Render new Elements
-var carList = document.querySelector("#carList");
-var carWheel = document.querySelector("#carWheel");
+// const carList = document.querySelector("#carList");
+// const carWheel = document.querySelector("#carWheel");
 //Submit Car Form & Remove Form
 form.addEventListener('submit', function (e) {
     car = new Car(plate.value.toUpperCase(), color.value.toUpperCase(), brand.value.toUpperCase());
-    if (plate.value == "") {
+    if (plate.value == "" && color.value == "" && brand.value == "") {
         plate.classList.add("is-invalid");
         document.getElementById("errorPlate").textContent = "El campo es obligatorio";
+        color.classList.add("is-invalid");
+        document.getElementById("errorColor").textContent = "El campo es obligatorio";
+        brand.classList.add("is-invalid");
+        document.getElementById("errorBrand").textContent = "El campo es obligatorio";
     }
-    else if (!(/^\d{4}[A-Z a-z]{3}/.test(plate.value))) {
+    else if (!(/^\d{4}[a-z]{3}/.test(plate.value))) {
         plate.classList.add("is-invalid");
         document.getElementById("errorPlate").textContent = "La matricula debe contener 4 numeros y 3 letras";
     }
-    else {
+    else if ((/^\d{4}[a-z]{3}/.test(plate.value))) {
+        document.getElementById("carPlate").textContent = "" + car.plate;
+        document.getElementById("carColor").textContent = "" + car.color;
+        document.getElementById("carBrand").textContent = "" + car.brand;
         carForm.remove();
         wheelApp.style.display = 'block';
     }
-    if ((/^\d{4}[a-z]{3}/.test(plate.value))) {
-        var div = document.createElement("div");
-        div.innerHTML = " \n    <strong>CAR:</strong><br>\n    <div class=\"row mb-3 pt-3\"> \n      <div class=\"form-group col-md-2\">\n         <strong>Plate</strong>: " + car.plate + "\n      </div>\n      <div class=\"form-group col-md-2\">\n        <strong>Color</strong>: " + car.color + "\n      </div>\n       <div class=\"form-group col-md-2\">\n          <strong>Brand</strong>: " + car.brand + "<br>\n       </div>\n    </div>\n    </div>";
-        carList === null || carList === void 0 ? void 0 : carList.appendChild(div);
-    }
     e.preventDefault();
 });
+// (/^\d{4}[a-z]{3}/.test(plate.value))
 //Submit Wheel Form
 wheelForm.addEventListener('submit', function (e) {
+    //Wheel Inputs Form
+    var marcaRueda1 = document.querySelector("#marcaRueda1");
+    var marcaRueda2 = document.querySelector("#marcaRueda2");
+    var marcaRueda3 = document.querySelector("#marcaRueda3");
+    var marcaRueda4 = document.querySelector("#marcaRueda4");
+    var diametro1 = document.querySelector("#diametro1");
+    var diametro2 = document.querySelector("#diametro2");
+    var diametro3 = document.querySelector("#diametro3");
+    var diametro4 = document.querySelector("#diametro4");
     var marca1 = marcaRueda1.value.toUpperCase();
     var marca2 = marcaRueda2.value.toUpperCase();
     var marca3 = marcaRueda3.value.toUpperCase();
@@ -53,67 +56,87 @@ wheelForm.addEventListener('submit', function (e) {
     var wheel2 = diametro2.value;
     var wheel3 = diametro3.value;
     var wheel4 = diametro4.value;
-    if (marcaRueda1.value == "") {
+    var displayWheel = document.querySelector('#displayWheel');
+    if (marcaRueda1.value == "" && marcaRueda2.value == "" && marcaRueda3.value == "" && marcaRueda1.value == "") {
         marcaRueda1.classList.add("is-invalid");
         document.getElementById("errorRueda1").textContent = "El campo es obligatorio";
+        marcaRueda2.classList.add("is-invalid");
+        document.getElementById("errorRueda2").textContent = "El campo es obligatorio";
+        marcaRueda3.classList.add("is-invalid");
+        document.getElementById("errorRueda3").textContent = "El campo es obligatorio";
+        marcaRueda4.classList.add("is-invalid");
+        document.getElementById("errorRueda4").textContent = "El campo es obligatorio";
     }
-    if (diametro1.value == "") {
+    if (diametro1.value == "" && diametro2.value == "" && diametro3.value == "" && diametro4.value == "") {
         diametro1.classList.add("is-invalid");
         document.getElementById("errorDiametro1").textContent = "El campo es obligatorio";
+        diametro2.classList.add("is-invalid");
+        document.getElementById("errorDiametro2").textContent = "El campo es obligatorio";
+        diametro3.classList.add("is-invalid");
+        document.getElementById("errorDiametro3").textContent = "El campo es obligatorio";
+        diametro4.classList.add("is-invalid");
+        document.getElementById("errorDiametro4").textContent = "El campo es obligatorio";
     }
     else if (parseFloat(diametro1.value) > 2 || parseFloat(diametro1.value) < 0.4) {
         diametro1.classList.add("is-invalid");
         document.getElementById("errorDiametro1").textContent = "El diámetro no es correcto";
     }
-    if (marcaRueda2.value == "") {
-        marcaRueda2.classList.add("is-invalid");
-        document.getElementById("errorRueda2").textContent = "El campo es obligatorio";
-    }
-    if (diametro2.value == "") {
-        diametro2.classList.add("is-invalid");
-        document.getElementById("errorDiametro2").textContent = "El campo es obligatorio";
-    }
     else if (parseFloat(diametro2.value) > 2 || parseFloat(diametro2.value) < 0.4) {
         diametro2.classList.add("is-invalid");
         document.getElementById("errorDiametro2").textContent = "El diámetro no es correcto";
     }
-    if (marcaRueda3.value == "") {
-        marcaRueda3.classList.add("is-invalid");
-        document.getElementById("errorRueda3").textContent = "El campo es obligatorio";
-    }
-    if (diametro3.value == "") {
-        diametro3.classList.add("is-invalid");
-        document.getElementById("errorDiametro3").textContent = "El campo es obligatorio";
-    }
     else if (parseFloat(diametro3.value) > 2 || parseFloat(diametro3.value) < 0.4) {
         diametro3.classList.add("is-invalid");
         document.getElementById("errorDiametro3").textContent = "El diámetro no es correcto";
-    }
-    if (marcaRueda4.value == "") {
-        marcaRueda4.classList.add("is-invalid");
-        document.getElementById("errorRueda4").textContent = "El campo es obligatorio";
-    }
-    if (diametro4.value == "") {
-        diametro4.classList.add("is-invalid");
-        document.getElementById("errorDiametro4").textContent = "El campo es obligatorio";
     }
     else if (parseFloat(diametro4.value) > 2 || parseFloat(diametro4.value) < 0.4) {
         diametro4.classList.add("is-invalid");
         document.getElementById("errorDiametro4").textContent = "El diámetro no es correcto";
     }
     else if (true) {
-        var div = document.createElement("div");
-        div.innerHTML = "<strong>Wheels:</strong><br>\n    <div class=\"row mb-4\"> \n      <div class=\"form-group col-md-2\">\n         <strong>Marca Rueda1</strong>: " + marca1 + "\n         <strong>Diametro</strong>: " + wheel1 + "\n      </div>\n      <div class=\"form-group col-md-2\">\n        <strong>Marca Rueda 2</strong>:" + marca2 + "\n        <strong>Diametro</strong>:  " + wheel2 + "\n      </div>\n    <div class=\"form-group col-md-2\">\n      <strong>Marca Rueda 3</strong>: " + marca3 + "\n      <strong>Diametro</strong>:  " + wheel3 + "\n    </div>\n    <div class=\"form-group col-md-2\">\n        <strong>Marca Rueda 4</strong>: " + marca4 + "\n       <strong>Diametro</strong>:  " + wheel4 + "\n    </div>\n       </div>\n    </div>";
-        carWheel === null || carWheel === void 0 ? void 0 : carWheel.appendChild(div);
-        if (true) {
-            wheelForm.reset();
-        }
+        displayWheel.style.display = 'block';
+        document.getElementById("brand1").textContent = "" + marca1;
+        document.getElementById("rueda1").textContent = "" + wheel1;
+        document.getElementById("brand2").textContent = "" + marca2;
+        document.getElementById("rueda2").textContent = "" + wheel2;
+        document.getElementById("brand3").textContent = "" + marca3;
+        document.getElementById("rueda3").textContent = "" + wheel3;
+        document.getElementById("brand4").textContent = "" + marca4;
+        document.getElementById("rueda4").textContent = "" + wheel4;
+        wheelForm.reset();
     }
     e.preventDefault();
 });
 // //Blur effect
+form.addEventListener('blur', function (e) {
+    if (e.target.value != "")
+        e.target.classList.remove('is-invalid');
+}, true);
 wheelForm.addEventListener('blur', function (e) {
     if (e.target.value != "")
         e.target.classList.remove('is-invalid');
 }, true);
 // console.log(new Wheel(diame.value,wheel1));
+//    const div = document.createElement("div");
+//    div.innerHTML = `<strong>Wheels:</strong><br>
+//  <div class="row mb-4"> 
+//    <div class="form-group col-md-2">
+//       <strong>Marca Rueda1</strong>: ${marca1}
+//       <strong>Diametro</strong>: ${wheel1}
+//    </div>
+//    <div class="form-group col-md-2">
+//      <strong>Marca Rueda 2</strong>:${marca2}
+//      <strong>Diametro</strong>:  ${wheel2}
+//    </div>
+//  <div class="form-group col-md-2">
+//    <strong>Marca Rueda 3</strong>: ${marca3}
+//    <strong>Diametro</strong>:  ${wheel3}
+//  </div>
+//  <div class="form-group col-md-2">
+//      <strong>Marca Rueda 4</strong>: ${marca4}
+//     <strong>Diametro</strong>:  ${wheel4}
+//  </div>
+//     </div>
+//  </div>`
+//       ;
+//    carWheel?.appendChild(div);
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -10,65 +10,53 @@ const plate = document.querySelector("#plate") as HTMLInputElement;
 const brand = document.querySelector("#brand") as HTMLInputElement;
 const color = document.querySelector("#color") as HTMLInputElement;
 
-//Inputs wheel Form
-
-const marcaRueda1 = document.querySelector("#marcaRueda1") as HTMLInputElement;
-const marcaRueda2 = document.querySelector("#marcaRueda2") as HTMLInputElement;
-const marcaRueda3 = document.querySelector("#marcaRueda3") as HTMLInputElement;
-const marcaRueda4 = document.querySelector("#marcaRueda4") as HTMLInputElement;
-
-const diametro1 = document.querySelector("#diametro1") as HTMLInputElement;
-const diametro2 = document.querySelector("#diametro2") as HTMLInputElement;
-const diametro3 = document.querySelector("#diametro3") as HTMLInputElement;
-const diametro4 = document.querySelector("#diametro4") as HTMLInputElement;
-
-
-
-
 // Render new Elements
-const carList = document.querySelector("#carList");
-const carWheel = document.querySelector("#carWheel");
+// const carList = document.querySelector("#carList");
+// const carWheel = document.querySelector("#carWheel");
 
 //Submit Car Form & Remove Form
 form.addEventListener('submit', (e: Event) => {
    car = new Car(plate.value.toUpperCase(), color.value.toUpperCase(), brand.value.toUpperCase());
-   if (plate.value == "") {
+   if (plate.value == "" && color.value == "" && brand.value == "") {
       plate.classList.add("is-invalid");
       document.getElementById("errorPlate")!.textContent = "El campo es obligatorio";
-   } else if (!(/^\d{4}[A-Z a-z]{3}/.test(plate.value))) {
+      color.classList.add("is-invalid");
+      document.getElementById("errorColor")!.textContent = "El campo es obligatorio";
+      brand.classList.add("is-invalid");
+      document.getElementById("errorBrand")!.textContent = "El campo es obligatorio";
+   } else if (!(/^\d{4}[a-z]{3}/.test(plate.value))) {
       plate.classList.add("is-invalid");
       document.getElementById("errorPlate")!.textContent = "La matricula debe contener 4 numeros y 3 letras";
-   } else {
+
+   }
+   else if ((/^\d{4}[a-z]{3}/.test(plate.value))) {
+      document.getElementById("carPlate")!.textContent = `${car.plate}`
+      document.getElementById("carColor")!.textContent = `${car.color}`
+      document.getElementById("carBrand")!.textContent = `${car.brand}`
       carForm.remove();
       wheelApp.style.display = 'block';
    }
-   if ((/^\d{4}[a-z]{3}/.test(plate.value))) {
-      const div = document.createElement("div");
-      div.innerHTML = ` 
-    <strong>CAR:</strong><br>
-    <div class="row mb-3 pt-3"> 
-      <div class="form-group col-md-2">
-         <strong>Plate</strong>: ${car.plate}
-      </div>
-      <div class="form-group col-md-2">
-        <strong>Color</strong>: ${car.color}
-      </div>
-       <div class="form-group col-md-2">
-          <strong>Brand</strong>: ${car.brand}<br>
-       </div>
-    </div>
-    </div>`
-         ;
-      carList?.appendChild(div);
 
-   }
    e.preventDefault();
 
 });
+// (/^\d{4}[a-z]{3}/.test(plate.value))
 
 //Submit Wheel Form
 
 wheelForm.addEventListener('submit', (e: Event) => {
+   //Wheel Inputs Form
+   const marcaRueda1 = document.querySelector("#marcaRueda1") as HTMLInputElement;
+   const marcaRueda2 = document.querySelector("#marcaRueda2") as HTMLInputElement;
+   const marcaRueda3 = document.querySelector("#marcaRueda3") as HTMLInputElement;
+   const marcaRueda4 = document.querySelector("#marcaRueda4") as HTMLInputElement;
+
+   const diametro1 = document.querySelector("#diametro1") as HTMLInputElement;
+   const diametro2 = document.querySelector("#diametro2") as HTMLInputElement;
+   const diametro3 = document.querySelector("#diametro3") as HTMLInputElement;
+   const diametro4 = document.querySelector("#diametro4") as HTMLInputElement;
+
+
    let marca1 = marcaRueda1.value.toUpperCase();
    let marca2 = marcaRueda2.value.toUpperCase();
    let marca3 = marcaRueda3.value.toUpperCase();
@@ -79,96 +67,67 @@ wheelForm.addEventListener('submit', (e: Event) => {
    let wheel3 = diametro3.value;
    let wheel4 = diametro4.value;
 
-   if (marcaRueda1.value == "") {
+   const displayWheel: any = document.querySelector('#displayWheel');
+
+   if (marcaRueda1.value == "" && marcaRueda2.value == "" && marcaRueda3.value == "" && marcaRueda1.value == "") {
       marcaRueda1.classList.add("is-invalid");
       document.getElementById("errorRueda1")!.textContent = "El campo es obligatorio";
-   }
-   if (diametro1.value == "") {
-      diametro1.classList.add("is-invalid");
-      document.getElementById("errorDiametro1")!.textContent = "El campo es obligatorio";
-
-   } else if (parseFloat(diametro1.value) > 2 || parseFloat(diametro1.value) < 0.4) {
-      diametro1.classList.add("is-invalid");
-      document.getElementById("errorDiametro1")!.textContent = "El diámetro no es correcto";
-
-   }
-   if (marcaRueda2.value == "") {
       marcaRueda2.classList.add("is-invalid");
       document.getElementById("errorRueda2")!.textContent = "El campo es obligatorio";
-
-   }
-   if (diametro2.value == "") {
-      diametro2.classList.add("is-invalid");
-      document.getElementById("errorDiametro2")!.textContent = "El campo es obligatorio";
-
-   } else if (parseFloat(diametro2.value) > 2 || parseFloat(diametro2.value) < 0.4) {
-      diametro2.classList.add("is-invalid");
-      document.getElementById("errorDiametro2")!.textContent = "El diámetro no es correcto";
-
-   }
-   if (marcaRueda3.value == "") {
       marcaRueda3.classList.add("is-invalid");
       document.getElementById("errorRueda3")!.textContent = "El campo es obligatorio";
-
-   }
-   if (diametro3.value == "") {
-      diametro3.classList.add("is-invalid");
-      document.getElementById("errorDiametro3")!.textContent = "El campo es obligatorio";
-
-   } else if (parseFloat(diametro3.value) > 2 || parseFloat(diametro3.value) < 0.4) {
-      diametro3.classList.add("is-invalid");
-      document.getElementById("errorDiametro3")!.textContent = "El diámetro no es correcto";
-
-   }
-   if (marcaRueda4.value == "") {
       marcaRueda4.classList.add("is-invalid");
       document.getElementById("errorRueda4")!.textContent = "El campo es obligatorio";
-
    }
-   if (diametro4.value == "") {
+   if (diametro1.value == "" && diametro2.value == "" && diametro3.value == "" && diametro4.value == "") {
+      diametro1.classList.add("is-invalid");
+      document.getElementById("errorDiametro1")!.textContent = "El campo es obligatorio";
+      diametro2.classList.add("is-invalid");
+      document.getElementById("errorDiametro2")!.textContent = "El campo es obligatorio";
+      diametro3.classList.add("is-invalid");
+      document.getElementById("errorDiametro3")!.textContent = "El campo es obligatorio";
       diametro4.classList.add("is-invalid");
       document.getElementById("errorDiametro4")!.textContent = "El campo es obligatorio";
+   }
 
-   } else if (parseFloat(diametro4.value) > 2 || parseFloat(diametro4.value) < 0.4) {
+   else if (parseFloat(diametro1.value) > 2 || parseFloat(diametro1.value) < 0.4) {
+      diametro1.classList.add("is-invalid");
+      document.getElementById("errorDiametro1")!.textContent = "El diámetro no es correcto";
+   }
+   else if (parseFloat(diametro2.value) > 2 || parseFloat(diametro2.value) < 0.4) {
+      diametro2.classList.add("is-invalid");
+      document.getElementById("errorDiametro2")!.textContent = "El diámetro no es correcto";
+   }
+   else if (parseFloat(diametro3.value) > 2 || parseFloat(diametro3.value) < 0.4) {
+      diametro3.classList.add("is-invalid");
+      document.getElementById("errorDiametro3")!.textContent = "El diámetro no es correcto";
+   }
+   else if (parseFloat(diametro4.value) > 2 || parseFloat(diametro4.value) < 0.4) {
       diametro4.classList.add("is-invalid");
       document.getElementById("errorDiametro4")!.textContent = "El diámetro no es correcto";
 
-   } else if (true) {
-      const div = document.createElement("div");
-      div.innerHTML = `<strong>Wheels:</strong><br>
-    <div class="row mb-4"> 
-      <div class="form-group col-md-2">
-         <strong>Marca Rueda1</strong>: ${marca1}
-         <strong>Diametro</strong>: ${wheel1}
-      </div>
-      <div class="form-group col-md-2">
-        <strong>Marca Rueda 2</strong>:${marca2}
-        <strong>Diametro</strong>:  ${wheel2}
-      </div>
-    <div class="form-group col-md-2">
-      <strong>Marca Rueda 3</strong>: ${marca3}
-      <strong>Diametro</strong>:  ${wheel3}
-    </div>
-    <div class="form-group col-md-2">
-        <strong>Marca Rueda 4</strong>: ${marca4}
-       <strong>Diametro</strong>:  ${wheel4}
-    </div>
-       </div>
-    </div>`
-         ;
-
-
-      carWheel?.appendChild(div);
-      if (true) {
-         wheelForm.reset();
-      }
-
    }
-
+   else if (true) {
+      displayWheel.style.display = 'block';
+      document.getElementById("brand1")!.textContent = `${marca1}`
+      document.getElementById("rueda1")!.textContent = `${wheel1}`
+      document.getElementById("brand2")!.textContent = `${marca2}`
+      document.getElementById("rueda2")!.textContent = `${wheel2}`
+      document.getElementById("brand3")!.textContent = `${marca3}`
+      document.getElementById("rueda3")!.textContent = `${wheel3}`
+      document.getElementById("brand4")!.textContent = `${marca4}`
+      document.getElementById("rueda4")!.textContent = `${wheel4}`
+      wheelForm.reset();
+   }
    e.preventDefault();
 });
 
 // //Blur effect
+form.addEventListener('blur', function (e: any) {
+   if (e.target.value != "")
+      e.target.classList.remove('is-invalid');
+}, true);
+
 wheelForm.addEventListener('blur', function (e: any) {
    if (e.target.value != "")
       e.target.classList.remove('is-invalid');
@@ -177,3 +136,27 @@ wheelForm.addEventListener('blur', function (e: any) {
 // console.log(new Wheel(diame.value,wheel1));
 
 
+  //    const div = document.createElement("div");
+   //    div.innerHTML = `<strong>Wheels:</strong><br>
+   //  <div class="row mb-4"> 
+   //    <div class="form-group col-md-2">
+   //       <strong>Marca Rueda1</strong>: ${marca1}
+   //       <strong>Diametro</strong>: ${wheel1}
+   //    </div>
+   //    <div class="form-group col-md-2">
+   //      <strong>Marca Rueda 2</strong>:${marca2}
+   //      <strong>Diametro</strong>:  ${wheel2}
+   //    </div>
+   //  <div class="form-group col-md-2">
+   //    <strong>Marca Rueda 3</strong>: ${marca3}
+   //    <strong>Diametro</strong>:  ${wheel3}
+   //  </div>
+   //  <div class="form-group col-md-2">
+   //      <strong>Marca Rueda 4</strong>: ${marca4}
+   //     <strong>Diametro</strong>:  ${wheel4}
+   //  </div>
+   //     </div>
+   //  </div>`
+   //       ;
+
+   //    carWheel?.appendChild(div);
